f7a4eb919b0826221e80371f8d9c53e74bfff56d
Ed Page <eopage@byu.net>
2011-03-15 22:32:58 -0500

    Bump to 1.0.6

--- support/builddeb.py
+++ support/builddeb.py
@@ -27,8 +27,7 @@ __email__ = "eopage@byu.net"
 __version__ = constants.__version__
 __build__ = constants.__build__
 __changelog__ = """
-* Fixing some orientation bugs
-* Making log file rotate due to long release cycles
+* Fixing Maemo 4.1
 """
 
 
